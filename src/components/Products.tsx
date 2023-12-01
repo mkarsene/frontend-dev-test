226e5e3a71821c103f3a5849338a92b9cecbcbd2
Mwapsam <mwapsam@gmail.com>
2023-12-01 14:40:15 +0200

    Customize products grid

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,14 +1,27 @@
+import { Box, Image } from '@chakra-ui/react';
+import { Hit } from 'react-instantsearch-core';
 import { TypesenseItem } from '../types';
+import ProductDetails from './modals/ProductDetails';
 
-const Products = ({ hit }: { hit: TypesenseItem }) => {
+interface ProductsProps {
+  hit: Hit<TypesenseItem>;
+  className?: string;
+}
+
+const Products: React.FC<ProductsProps> = ({ hit }) => {
+  const image = `https://dghhemym84nng.cloudfront.net/bofrak-uploads/${hit.sku}.png`
   return (
-    <div>
-      {hit.name}
-      <img
-        src={`https://dghhemym84nng.cloudfront.net/bofrak-uploads/${hit.sku}.png`}
+    <Box position="relative">
+        <ProductDetails productName={hit.name} image={image} />
+      <Image
+        src={image}
         alt={hit.name}
+        boxSize="150px"
+        objectFit="cover"
       />
-    </div>
+      {hit.name}
+
+    </Box>
   );
 };
 
