593d7bf7316fc99e7d10348dd1078b97b48c0bf9
Desire <desireirankundawilliams@gmail.com>
2023-12-02 05:55:59 +0800

    able to display users on the login page

--- dev-test/src/Auth/Login.tsx
+++ dev-test/src/Auth/Login.tsx
@@ -1,10 +1,39 @@
 import { Button, Input, InputGroup, InputRightElement, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Select, useDisclosure } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+
+interface User {
+    id: string;
+    name: string;
+
+}
 
 const Login = () => {
+    const [users, setUsers] = useState([])
+    console.log("user id", users);
+
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [errrorMessage, setErrorMessage] = useState<string>("")
     const { isOpen, onOpen, onClose } = useDisclosure()
     const [show, setShow] = React.useState(false)
     const handleClick = () => setShow(!show)
+
+
+    const getAllUsers = async () => {
+        setIsLoading(true)
+        axios.get('https://bewty7mih9.execute-api.eu-central-1.amazonaws.com/users', { params: { store_id: "ea7aa965-bef1-4c38-b2b3-e62c865b5a7a", limit: 50 } }
+        ).then((res) => {
+            setUsers(res.data.employees)
+            setIsLoading(false)
+        }).catch((error) => {
+            setIsLoading(false)
+            setErrorMessage(error.message)
+
+        })
+    }
+    useEffect(() => {
+        getAllUsers()
+    }, [])
     return (
         <div>
             <Button onClick={onOpen}>Open Modal</Button>
@@ -13,10 +42,11 @@ const Login = () => {
                 <ModalOverlay />
                 <ModalContent>
                     <ModalHeader>Enter Password</ModalHeader>
-                    <Select placeholder='Select option' p="10px">
-                        <option value='option1'>Option 1</option>
-                        <option value='option2'>Option 2</option>
-                        <option value='option3'>Option 3</option>
+                    <Select placeholder='Select User' p="10px">
+                        {users.map((user: User) => (
+                            <option key={user.id} value={user.id}>{user.name}</option>
+
+                        ))}
                     </Select>
 
                     <InputGroup size='md' p="10px">
